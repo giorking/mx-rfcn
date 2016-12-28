c3fcad450a6303369cf99152d2f684e6af14ce11
Terry Chen <hahakuku@hotmail.com>
2016-12-27 22:59:48 -0800

    Update find_mxnet.py

--- find_mxnet.py
+++ find_mxnet.py
@@ -3,6 +3,5 @@ try:
 except ImportError:
 	import os, sys
 	curr_path = os.path.abspath(os.path.dirname(__file__))
-	# sys.path.append(os.path.join("/home/tairuic/Downloads/mxnet_pkg/mxnet/python"))
-	sys.path.append(os.path.join("/home/tairuic/Desktop/mxnet/python"))
+	sys.path.append(os.path.join("home/mxnet/python"))
 	import mxnet as mx
